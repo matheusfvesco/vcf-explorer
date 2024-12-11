92d0610029db302cd78f54513b743b4942fb7b22
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2024-12-11 16:20:05 -0300

    Update run.sh

--- src/api/run.sh
+++ src/api/run.sh
@@ -2,17 +2,4 @@
 
 uv run snakemake
 
-# starts n APIs for each sample
-
-start_port=4000
-
-# Counter for ports
-port=$start_port
-
-for file in data/*.tsv; do
-  # Run the command in the background
-  uv run --package api src/api/api/serve.py $file --port $port &
-  
-  # Increment the port number
-  ((port++))
-done
+uv run fastapi run serve.py --port 4000
