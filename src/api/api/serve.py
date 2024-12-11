31e591ac4a49aaf91651fff4c42b23d2a4253631
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2024-12-11 16:19:50 -0300

    Update endpoints

--- src/api/api/serve.py
+++ src/api/api/serve.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI, HTTPException
 import polars as pl
 from pathlib import Path
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("sample", type=str)
-parser.add_argument("--port", type=str)
-args = parser.parse_args()
-
-# first tsv file found
-#df_path = sorted(Path("data").rglob("*.tsv"))[0]
-df_path = Path(args.sample)
-variants_df = pl.read_csv(df_path, separator="\t")
-variants_df = variants_df.sort("freq",nulls_last=True)
+df_paths = sorted(Path("data").rglob("*.tsv"))
+variants_dfs: dict[str, pl.DataFrame] = {
+    df_path.stem: pl.read_csv(df_path, separator="\t").sort("freq",nulls_last=True) for df_path in df_paths
+}
 
 tags_metadata = [
     {
@@ -26,16 +19,16 @@ tags_metadata = [
 ]
 
 description = f"""
-# API for the {df_path.stem} sample
+# API for the {', '.join(list(variants_dfs.keys()))} samples
 
 This API is designed to be used for accessing and filtering the variants in the sample.
 """
 
 app = FastAPI(
     openapi_tags=tags_metadata,
-    title=df_path.stem,
+    title="vcf api",
     description=description,
-    summary=f"API for the {df_path.stem} sample",
+    summary=f"API for the {', '.join(list(variants_dfs.keys()))} samples",
     version="0.0.1",
 )
 
@@ -50,9 +43,9 @@ app = FastAPI(
 )
 def read_root():
     return {
-        "info": f"Serving API for the {df_path.stem} sample",
-        "sample": df_path.stem,
-        "num_SNPs": len(variants_df),
+        "info": f"Serving API for the {', '.join(list(variants_dfs.keys()))} samples",
+        "samples": list(variants_dfs.keys()),
+        "num_SNPs": {sample: len(variants_dfs[sample]) for sample in variants_dfs},
     }
 
 @app.get(
@@ -65,57 +58,71 @@ def read_root():
     },
 )
 def meta():
-    return {
-        "num_SNPs": len(variants_df),
-        "dp": [variants_df["dp"].min(), variants_df["dp"].max()],
-        "freq": [variants_df["freq"].min(), variants_df["freq"].max()],
-        "male_freq": [variants_df["male_freq"].min(), variants_df["male_freq"].max()],
-        "female_freq": [variants_df["female_freq"].min(), variants_df["female_freq"].max()],
-    }
+    out = {}
+    for sample in variants_dfs:
+        out[sample] = {
+                "num_SNPs": len(variants_dfs[sample]),
+                "dp": [variants_dfs[sample]["dp"].min(), variants_dfs[sample]["dp"].max()],
+                "freq": [variants_dfs[sample]["freq"].min(), variants_dfs[sample]["freq"].max()],
+                "male_freq": [variants_dfs[sample]["male_freq"].min(), variants_dfs[sample]["male_freq"].max()],
+                "female_freq": [variants_dfs[sample]["female_freq"].min(), variants_dfs[sample]["female_freq"].max()],
+            }
+    return out
 
 @app.get(
-    "/variants",
-    summary="Retrieves the entire dataset", 
-    description="Retrieves the entire dataset",
+    "/samples",
+    summary="Lists the samples available", 
+    description="Lists the samples available",
     tags=["items"],
     responses={
         200: {"description": "Successful response with dataframe as a list of dicts"},
     },
 )
-def get_variants():
-    return variants_df.to_dicts()
+def samples():
+    return [sample for sample in variants_dfs]
 
 @app.get(
-    "/filter/{parameter}/{operator}/{value}",
+    "/variants/{sample}",
+    summary="Retrieves the entire dataset for a sample", 
+    description="Retrieves the entire dataset for a sample",
+    tags=["items"],
+    responses={
+        200: {"description": "Successful response with dataframe as a list of dicts"},
+    },
+)
+def get_variants(sample: str):
+    if sample not in variants_dfs:
+        raise HTTPException(status_code=404, detail="Sample not found")
+    
+    return {"sample": sample, "variants": variants_dfs[sample].to_dicts()}
+
+@app.get(
+    "/filter/{sample}/{parameter}/{operator}/{value}",
     summary="Filters the dataset", 
     description="Filters the dataset using the specified parameter, based on the specified operator and value",
     tags=["filter"],
     responses={
         200: {"description": "Successful response with dataframe as a list of dicts"},
         400: {"description": "User mistake on the query"},
+        404: {"description": "Sample not found"},
     },
 )
-def filter_variants(parameter: str, operator: str, value: float):
+def filter_variants(sample: str, parameter: str, operator: str, value: float):
     # hgvs	rsid	genes	freq	male_freq	female_freq	dp
     accepted_columns = ["freq", "male_freq", "female_freq", "dp"]
     accepted_params = ["gt", "lt", "eq"]
+    if sample not in variants_dfs:
+        raise HTTPException(status_code=404, detail="Sample not found")
     if parameter not in accepted_columns:
         raise HTTPException(status_code=400, detail=f"Invalid parameter: {parameter}. Must be one of: {accepted_columns}")
-
     if operator not in accepted_params:
         raise HTTPException(status_code=400, detail=f"Invalid operator. Must be one of: {accepted_params}")
-    if value < 0 or value > variants_df[parameter].max():
-        raise HTTPException(status_code=400, detail=f"Value {value} is out of range for parameter: {parameter}")
     
     if operator == "eq":
-        filtered_variants = variants_df.filter(pl.col(parameter) == value)
+        filtered_variants = variants_dfs[sample].filter(pl.col(parameter) == value)
     elif operator == "gt":
-        filtered_variants = variants_df.filter(pl.col(parameter) > value)
+        filtered_variants = variants_dfs[sample].filter(pl.col(parameter) >= value)
     elif operator == "lt":
-        filtered_variants = variants_df.filter(pl.col(parameter) < value)
+        filtered_variants = variants_dfs[sample].filter(pl.col(parameter) <= value)
 
-    return filtered_variants.to_dicts()
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=int(args.port))
+    return {"sample": sample, "variants": filtered_variants.to_dicts()}
