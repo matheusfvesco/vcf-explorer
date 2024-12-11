4d3d3b5f65de2bc6366a73b74125f21824098c66
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2024-12-11 16:19:28 -0300

    Update tests

--- src/api/api/test_serve.py
+++ src/api/api/test_serve.py
@@ -2,22 +2,25 @@ from serve import app
 from fastapi.testclient import TestClient
 import pytest
 import random
+import polars as pl
 
 client = TestClient(app)
 
 def test_variants():
-    response = client.get('/variants')
-    assert response.status_code == 200
+    for sample in client.get('/samples').json():
+        response = client.get(f'/variants/{sample}')
+        assert response.status_code == 200
 
 def test_meta():
     response = client.get('/meta')
     assert response.status_code == 200
     json_resp = response.json()
-    assert json_resp["num_SNPs"] > 0
-    assert len(json_resp["dp"]) == 2
-    assert len(json_resp["freq"]) == 2
-    assert len(json_resp["male_freq"]) == 2
-    assert len(json_resp["female_freq"]) == 2
+    for sample in client.get('/samples').json():
+        assert json_resp[sample]["num_SNPs"] > 0
+        assert len(json_resp[sample]["dp"]) == 2
+        assert len(json_resp[sample]["freq"]) == 2
+        assert len(json_resp[sample]["male_freq"]) == 2
+        assert len(json_resp[sample]["female_freq"]) == 2
 
 
 @pytest.mark.parametrize(
@@ -45,11 +48,77 @@ def test_meta():
 def test_filter_correct(param, op, val):
     response = client.get('/meta')
     json_resp = response.json()
-    min, max = tuple(json_resp[param])
-    if val == "int":
-        val = random.randint(min, max)
-    else:
-        val = random.uniform(min, max)
-    
-    response = client.get(f'/filter/{param}/{op}/{val}')
-    assert response.status_code == 200
+    for sample in client.get('/samples').json():
+        min, max = tuple(json_resp[sample][param])
+        if val == "int":
+            val = random.randint(min, max)
+        else:
+            val = random.uniform(min, max)
+        
+        response = client.get(f'/filter/{sample}/{param}/{op}/{val}')
+        assert response.status_code == 200
+
+@pytest.mark.parametrize(
+    "param", [
+        "dp", "freq", "male_freq", "female_freq"
+    ]
+)
+def test_filter_min_value(param):
+    for sample in client.get('/samples').json():
+
+        df = pl.from_dicts(client.get(f'/variants/{sample}').json())
+        num_SNPs = len(df.filter(pl.col(param).is_not_null()))
+        json_resp = client.get('/meta').json()
+        #num_SNPs = json_resp[sample]["num_SNPs"]
+        
+        # Ensure num_SNPs is greater than 0 to proceed
+        assert num_SNPs > 0
+        
+        min_value, max_value = tuple(json_resp[sample][param])
+        
+        # Calculate the threshold value, which is 10% less than the minimum value
+        threshold_value = max(min_value * 0.0001, 0)
+        threshold_value = min_value - threshold_value
+        
+        # Query the filter endpoint with the 'lt' operator to get values less than the threshold
+        response = client.get(f'/filter/{sample}/{param}/gt/{threshold_value}')
+        
+        # Ensure the response is successful
+        assert response.status_code == 200
+        
+        # Assert that the length of the returned variants equals num_SNPs
+        json_resp = response.json()
+        assert len(json_resp["variants"]) == num_SNPs
+
+@pytest.mark.parametrize(
+    "param", [
+        "dp", "freq", "male_freq", "female_freq"
+    ]
+)
+def test_filter_max_value(param):
+    for sample in client.get('/samples').json():
+
+        df = pl.from_dicts(client.get(f'/variants/{sample}').json())
+        num_SNPs = len(df.filter(pl.col(param).is_not_null()))
+        json_resp = client.get('/meta').json()
+        #num_SNPs = json_resp[sample]["num_SNPs"]
+        
+        # Ensure num_SNPs is greater than 0 to proceed
+        assert num_SNPs > 0
+        
+        min_value, max_value = tuple(json_resp[sample][param])
+        
+        # Calculate the threshold value, which is 10% less than the minimum value
+        threshold_value = max_value * 0.0001
+        threshold_value = max_value + threshold_value
+        
+        # Query the filter endpoint with the 'lt' operator to get values less than the threshold
+        response = client.get(f'/filter/{sample}/{param}/lt/{threshold_value}')
+        
+        # Ensure the response is successful
+        assert response.status_code == 200
+        
+        # Assert that the length of the returned variants equals num_SNPs
+        json_resp = response.json()
+        assert len(json_resp["variants"]) == num_SNPs
+
